f9376c0e8287ac3fd3ad41cc5156f553eff3a173
kessosan <souchet@wanadoo.fr>
2015-05-26 21:23:12 +0200

    add electricity consumption

--- src/app.js
+++ src/app.js
@@ -5,21 +5,22 @@
  */
 
 var UI = require('ui');
-//var Vector2 = require('vector2');
+var Vector2 = require('vector2');
 var ajax = require('ajax');
-var baseURL = 'http://domoticz.kessosan.info/json.htm?type=command';
+var baseURL = 'http://domoticz.kessosan.info/json.htm?';
+var Vibe = require('ui/vibe');
 
 var main = new UI.Card({
   title: 'Domopebble',
   icon: 'images/logo_domoticz_p.png',
-  subtitle: 'Bienvenue sur Domopebble',
-  body: 'appuyez sur un bouton.'
+  //subtitle: 'Bienvenue sur Domopebble',
+  body: 'appuyez sur select'
 });
-
 
 main.show();
 
 main.on('click', 'select', function(e) {
+  
   var menu = new UI.Menu({
     sections: [{
       items: [{
@@ -37,61 +38,132 @@ main.on('click', 'select', function(e) {
       }, {
         title: 'portail ON/OFF',
         icon: 'images/portail_p.png'
+      }, {
+        title: 'conso élec.',
+        icon: 'images/eclair_p.png'
       }]
     }]
   });
+  
+  menu.show();
+  
   menu.on('select', function(e) {
     var URL ='';
+    var device = '';
+    var action = '';
+    
+    //screen while waiting for request to be done
+    var waitingWindow = new UI.Window();
+    
+    // Text element to inform user
+    var text = new UI.Text({
+      position: new Vector2(0, 0),
+      size: new Vector2(144, 168),
+      text:'envoi de la requête ...',
+      font:'GOTHIC_28_BOLD',
+      color:'black',
+      textOverflow:'wrap',
+      textAlign:'center',
+      backgroundColor:'white'
+    });
     
     if(e.itemIndex===0) {
-      URL  = baseURL + '&param=switchlight&idx=16&switchcmd=On&level=0&passcode=';
+      URL  = baseURL + 'type=command&param=switchlight&idx=16&switchcmd=On&level=0&passcode=';
+      device = "halogène";
+      action = device+' ON';
     }
     
     if(e.itemIndex===1) {
-      URL  = baseURL + '&param=switchlight&idx=16&switchcmd=Off&level=0&passcode=';
+      URL  = baseURL + 'type=command&param=switchlight&idx=16&switchcmd=Off&level=0&passcode=';      
+      device = "halogène";
+      action = device+' OFF';
     }
   
     if(e.itemIndex===2) {
-      URL  = baseURL + '&param=switchlight&idx=21&switchcmd=On&level=0&passcode=';
+      URL  = baseURL + 'type=command&param=switchlight&idx=21&switchcmd=On&level=0&passcode=';
+      device = "prise 2"; 
+      action = device+' ON';
     }
     
     if(e.itemIndex===3) {
-      URL  = baseURL + '&param=switchlight&idx=21&switchcmd=Off&level=0&passcode=';
+      URL  = baseURL + 'type=command&param=switchlight&idx=21&switchcmd=Off&level=0&passcode=';
+      device = "prise 2"; 
+      action = device+' OFF';
     }
     
     if(e.itemIndex===4) {
-      URL  = baseURL + '&param=switchlight&idx=23&switchcmd=On&level=0&passcode=';
+      URL  = baseURL + 'type=command&param=switchlight&idx=23&switchcmd=On&level=0&passcode=';
+      device = "portail"; 
+      action = 'ouv./ferm. '+device;
     }
+    
+    if(e.itemIndex===5) {
+      URL  = baseURL + 'type=devices&filter=utility&used=true&order=Name&plan=0';
+      device = "électricité"; 
+      action = 'consommation '+device;
+    }
+    
+    waitingWindow.add(text);
+    waitingWindow.show();
     
     // Make the request
     ajax(
       {
         url: URL,
-        type: 'json'/*,
-        headers: {
-          Cookie : 'SID=',
-          Connection: 'keep-alive'
-        }*/
+        type: 'json'
       },
       function(data) {
         // Success!
-        //console.log('Light is ON!');
+        var result = data.status;
+        
+        if(data.result[0].Type=='Energy') {
+          result = data.result[0].Usage;
+        } else {
+          result = data.status;
+        }
+        
+        console.log(action + ' ' +data.status +" !");
         console.log(JSON.stringify(data));
+        
+        // Add to splashWindow and show        
+        main.subtitle(action);
+        main.body(result);
+        main.show();
+        
+        Vibe.vibrate('short');        
+        
       },
-      function(error) {
+      function(error,data) {
         // Failure!
         //console.log('Failed to lightning '+ error);
         console.log(JSON.stringify(data));
+        
+        main.subtitle(action);
+        //main.body(data.status +'('+error+')');
+        main.body("erreur"); 
+        main.show();
+        
+        Vibe.vibrate('long');  
       }
     );
     
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+    //console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    //console.log('The item is titled "' + e.item.title + '"');
     
   });
 
-  menu.show();
 });
+
+/*function parseJSONResponse(data){
+  var result = null;
+  var parsedJSON = JSON.parse(data.result);
+  
+  for (var i=0;i<parsedJSON.length;i++) {
+    alert(parsedJSON[0].Id);
+  }
+
+  return result;
+}*/
 
 //menu.items(1, [ { title: 'Prise commandee 2 ON' }, { title: 'Prise commandee 2 OFF' } ]);
 
